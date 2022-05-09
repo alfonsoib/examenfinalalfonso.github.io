c2d62ff22dfb216a3f701107a9b0bfb6b3fbcbea
alfonsoib <96416393+alfonsoib@users.noreply.github.com>
2022-05-09 11:02:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Bienvenido al examen final");
+let person = prompt("Inserte aquí su nombre", "Alberto Menéndez Blanco");
+
+if (person != person) {
+  document.getElementById("demo").innerHTML =
+  "¡Hola " + person + "! ¿Qué tal estaás hoy?";
+}
